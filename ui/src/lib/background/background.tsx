03f61cd009123ef67fc5696d0c94cf96f975f804
Paulo Roberto Tessarolli Junior <paulortesjr@gmail.com>
2023-04-14 17:44:53 -0300

    change in background

--- ui/src/lib/background/background.tsx
+++ ui/src/lib/background/background.tsx
@@ -1,40 +1,10 @@
-import { useEffect, useMemo, useState } from 'react';
 import styles from './background.module.scss';
-import angularLogo from './images/angular.svg';
-import reactLogo from './images/react.svg';
-import svelteLogo from './images/svelte.svg';
-import flutterLogo from './images/flutter.svg';
+import bg from './images/bg.jpg';
 
 export function Background() {
-  const images = useMemo(
-    () => [angularLogo, reactLogo, svelteLogo, flutterLogo],
-    []
-  );
-  const [currentImageIndex, setCurrentImageIndex] = useState(0);
-  const [opacity, setOpacity] = useState(1);
-
-  useEffect(() => {
-    const intervalId = setInterval(() => {
-      setOpacity(0);
-      setTimeout(() => {
-        setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
-        setOpacity(1);
-      }, 500);
-    }, 3000);
-
-    return () => clearInterval(intervalId);
-  }, [images]);
-
   return (
     <div className={styles['background']}>
-      <img
-        className={styles['background-image']}
-        width={400}
-        height={400}
-        src={images[currentImageIndex]}
-        alt="logo slider"
-        style={{ opacity, transition: 'opacity 0.5s' }}
-      />
+      <img src={bg} alt="background" className={styles['backgrounds']} />
     </div>
   );
 }
